4bf94d09e6b9370d6b5cf88c6372ddd617edc8af
jeong0300 <dltnwjd8898@naver.com>
2025-01-24 20:52:06 +0900

    Feat: main 하는 중

--- js/bearCraftShop.js
+++ js/bearCraftShop.js
@@ -1,29 +1,27 @@
 const mainContainer = document.getElementById("mainContainer");
 
+const headDiv = document.createElement("div");
+
 const div = document.createElement("div");
+
+headDiv.innerHTML = "<h2> PRODUCT </h2><hr/>";
+mainContainer.appendChild(headDiv);
+
+let saveData = JSON.parse(window.localStorage.getItem("saveData")) || [];
 
 // 새로고침에도 테이블 유지
 function addTr() {
   const rows = saveData.map((data) => {
-    const tr = document.createElement("tr");
-    const tbody = document.createElement("tbody");
     const priceFin = Number(data.price).toLocaleString();
 
-    tbody.innerHTML = `
-    <tr id="${data.id}" data-id="${data.id}">
-      <td><img src="${data.img}" /></td>
-      <td><div> ${data.name} </div></td> 
-      <td><div> ${priceFin} </div></td>
-      <td><div> ${data.detail} </div></td> 
-      <td><button onclick="modifyList(${data.id})" id="modiBtn"> 수정 </button> <button onclick="deleteList()" data-id="${data.id}"> 삭제 </button></td>
-    </tr>`;
-
-    table.appendChild(tbody);
-
-    return tr;
+    console.log(data);
+    div.innerHTML = `
+    <div class="box" id="${data.id}" data-id="${data.id}">
+      <div><img src="${data.img}" /></div>
+      <div> ${data.name}</div>
+      <div> ${priceFin} </div>
+    </div>`;
   });
-
-  rows.map((tr) => table.appendChild(tr));
 }
 
 window.onload = function () {
